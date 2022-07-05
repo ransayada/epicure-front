dde78ef91ee279372f1c90887e1ed5f92ee4390d
ransayada <46240258+ransayada@users.noreply.github.com>
2022-07-05 12:02:15 +0300

    finish mobile our icons

--- src/pages/home-page/home-page-our-icons/OurIcons.style.ts
+++ src/pages/home-page/home-page-our-icons/OurIcons.style.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 export const OurIconsContainer = styled.div`
-    border: 1px solid red;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     height: 70vh;
     width: 100%;
     background-color: #fafafa;
+`;
+
+export const IconCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
 `;
--- src/pages/home-page/home-page-our-icons/OurIcons.tsx
+++ src/pages/home-page/home-page-our-icons/OurIcons.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { OurIconsContainer } from './OurIcons.style';
+import { OurIconsContainer, IconCard } from './OurIcons.style';
+import { default as spicyIcon } from '../../../assets/home-page-images/spicy-icon.svg';
+import { default as vegeIcon } from '../../../assets/home-page-images/vege-icon.svg';
+import { default as veganIcon } from '../../../assets/home-page-images/vegan-icon.svg';
 
 export const OurIcons: React.FC = () => {
     return (
         <>
-            <OurIconsContainer>OurIcons</OurIconsContainer>
+            <OurIconsContainer>
+                <p>OUR ICONS:</p>
+                <IconCard>
+                    <img src={spicyIcon} alt="" />
+                    <p>Spicy</p>
+                </IconCard>
+                <IconCard>
+                    <img src={vegeIcon} alt="" />
+                    <p>Vegetarian</p>
+                </IconCard>
+                <IconCard>
+                    <img src={veganIcon} alt="" />
+                    <p>Vegan</p>
+                </IconCard>
+            </OurIconsContainer>
         </>
     );
 };
